7b208dda50b3eab5ed710f3979782a6310e8bd56
ahaydeid <adi.hadi270@gmail.com>
2025-05-11 23:00:57 +0700

    Commit pertama

---
+++ assets/javascript/script.js
@@ -0,0 +1,13 @@
+// Template String
+// let namaAwal = "Ahadi";
+// let namaAkhir = "Hadi";
+// function sapa(nama){
+//     return `adalah ${nama}`;
+// }
+// console.log(`Hallo, nama lengkap saya ${sapa("Ahadi")}, dan nama panggilan saya adalah ${namaAkhir}`);
+
+//Ternary Operator
+// const user = "Admin";
+// let result = (user == "Admin") ? "Selamat datang admin" : "User tidak ditemukan";
+// console.log(result);
+
